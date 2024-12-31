cf235d4cad40297642040fb7acd784ec22544c6e
imag-er <2274265784@qq.com>
2024-12-31 12:38:50 +0800

    init

--- backend/utils/portscan_test.go
+++ backend/utils/portscan_test.go
@@ -1,17 +1,16 @@
 package utils
 
 import (
-	"log"
 	"testing"
 )
 
 func TestPortscan(t *testing.T) {
 	// portscan()
-	c := ACKScan("127.0.0.1")
-	for i := range c {
-		log.Println("port: ", i)
-	}
+	// c := ACKScan("127.0.0.1")
+	// for i := range c {
+	// 	log.Println("port: ", i)
+	// }
 
-	log.Println("TestPortscan")
+	// log.Println("TestPortscan")
 
 }
